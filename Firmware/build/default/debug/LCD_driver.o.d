0ab24f6edd27d8d9e285e434e5120892b879002f
lmeyer <lmeyer@student.42.fr>
2017-06-20 12:02:21 +0200

    SPI + Cleaning (#7)
    * mode sequentiel OK
    
    * marche en non bloquant
    
    * driver lcd quasi pret
    
    * putstr marche sa mere git add -A!
    
    * cleaning

---
+++ Firmware/build/default/debug/LCD_driver.o.d
@@ -0,0 +1,4 @@
+build/default/debug/LCD_driver.o:  \
+LCD_driver.c  \
+0x2a002a.h  \
+charset.h 
---
+++ Firmware/build/default/debug/main_encoder_driver.o.d
@@ -0,0 +1,3 @@
+build/default/debug/main_encoder_driver.o:  \
+main_encoder_driver.c  \
+ 0x2a002a.h 
